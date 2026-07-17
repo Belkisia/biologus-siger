722f27562ae517be9f5c67d4b097ddbd14c40df9
Belkisia <realfarma2011@gmail.com>
2026-07-17 19:35:39 -0300

    fix: remover asset antigo

--- .vercel/output/static/assets/index-CY7_g7Ga.js
+++
@@ -1,1 +0,0 @@
-import{r,a8 as s}from"./index-RcKNsen5.js";import{b as o}from"./index-DaH5ksAb.js";var c=s[" useId ".trim().toString()]||(()=>{}),n=0;function d(t){const[e,u]=r.useState(c());return o(()=>{u(a=>a??String(n++))},[t]),t||(e?`radix-${e}`:"")}function m(t){const e=r.useRef(t);return r.useEffect(()=>{e.current=t}),r.useMemo(()=>(...u)=>e.current?.(...u),[])}export{m as a,d as u};
